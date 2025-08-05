6b404263848ada5895e17e305dc6fa9b9fd61dca
Maria Carolina <mariacarolinatech@gmail.com>
2025-08-05 18:58:59 -0300

    Update vite.config.js

--- ViagemImpacta/frontend/vite.config.js
+++ ViagemImpacta/frontend/vite.config.js
@@ -1,17 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite'; // Importe este
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), 
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
