647e3eeb33fda9b412d4536d1cb50ea0dde0404a
glorylaflare <juninho_toh@hotmail.com>
2025-08-07 17:22:14 -0300

    refactor: atualização da errorviewmodel

---
+++ ViagemImpacta/backend/ViagemImpacta/Setup/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ViagemImpacta.Setup
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
